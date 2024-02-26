62727c114a4cd88c6c5d2aa6e8f80013312b187f
Clarissa Lynn <clarissalynn.5.cl@gmail.com>
2024-02-26 17:04:00 +0800

    partial function: filter

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,5 @@
 class DogsController < ApplicationController
+  def index
+    @dogs = Dog.by_breed_and_personality(params[:breed], params[:personality])
+  end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -5,4 +5,11 @@ class Dog < ApplicationRecord
   validates :personality, presence: true
   validates :photos, presence: true
   validates :date_of_birth, presence: true
+
+  def self.by_breed_and_personality(breed = nil, personality = nil)
+    return where(breed: breed, personality: personality) if breed && personality
+    return where(breed: breed) if breed
+    return where(personality: personality) if personality
+    all
+  end
 end
