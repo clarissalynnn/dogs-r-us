84bad199b6642c437153b6c9b7b437c790bf4ec0
Clarissa Lynn <clarissalynn.5.cl@gmail.com>
2024-02-26 16:12:43 +0800

    devise set up and seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,48 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+puts 'Destroying existing data'
+Booking.destroy_all
+Dog.destroy_all
+User.destroy_all
+
+puts 'Creating 5 users'
+5.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    password: 'password'
+  )
+  user.save!
+end
+puts 'Created 5 users'
+
+puts 'Creating 5 dogs'
+5.times do
+  dog = Dog.new(
+    name: Faker::Fantasy::Tolkien.character,
+    breed: Faker::Creature::Dog.breed,
+    personality: Faker::Creature::Dog.meme_phrase,
+    date_of_birth: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
+    photos: "https://source.unsplash.com/random/?dog",
+    owner: User.all.sample(1).first
+    )
+  dog.save!
+end
+puts 'Created 5 dogs'
+
+puts 'Creating 5 bookings'
+5.times do
+  booking = Booking.new(
+    user: User.all.sample(1).first,
+    dog: Dog.all.sample(1).first,
+    start_date: Faker::Date.between(from: '2014-09-23', to: '2014-09-25'),
+    end_date: Faker::Date.between(from: '2015-09-26', to: '2015-10-25'),
+    status: 'Approved'
+  )
+  booking.save!
+end
+puts 'Created 5 bookings'
+puts 'Finished creating 5 users, dogs, and bookings'
