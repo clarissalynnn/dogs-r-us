f91b6c633cadd17916fe1e370bb3150af42b87c6
Roeen Acal <roeenacal@gmail.com>
2024-02-27 22:46:54 +0800

    fix API credentials, add secret key, fix omni callback controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
-  devise :omniauthable, omniauth_providers: [:google_oauth2]
+  # devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   # You should also create an action method in this controller like this:
   # def twitter
@@ -29,27 +29,27 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # end
 
   #Google Auth
-    def google_oauth2
-     user = User.from_google(from_google_params)
+  def google_oauth2
+    user = User.from_google(from_google_params)
 
-     if user.present?
-       sign_out_all_scopes
-       flash[:notice] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-       sign_in_and_redirect user, event: :authentication
-     else
-       flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
-       redirect_to new_user_session_path
-     end
+    if user.present?
+      sign_out_all_scopes
+      flash[:notice] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+      sign_in_and_redirect user, event: :authentication
+    else
+      flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
+      redirect_to new_user_session_path
     end
+  end
 
-    def from_google_params
-      @from_google_params ||= {
-        uid: auth.uid,
-        email: auth.info.email
-      }
-    end
+  def from_google_params
+    @from_google_params ||= {
+      uid: auth.uid,
+      email: auth.info.email
+    }
+  end
 
-    def auth
-      @auth ||= request.env['omniauth.auth']
-    end
+  def auth
+    @auth ||= request.env['omniauth.auth']
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,5 +310,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :google_oauth2, 'GOOGLE_OAUTH_CLIENT_ID', 'GOOGLE_OAUTH_CLIENT_SECRET'
+  config.omniauth :google_oauth2, '506126752849-suu2l3sj6kvavhkurjvhfri0l5pqhng8.apps.googleusercontent.com', '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'
 end
