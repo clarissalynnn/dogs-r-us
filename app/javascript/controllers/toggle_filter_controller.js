5459525e714c5eeca040c49a36bac56047094f57
Clarissa Lynn <clarissalynn.5.cl@gmail.com>
2024-02-28 16:36:48 +0800

    the checkboxes' value can now be accessed. need to fetch and return

--- app/javascript/controllers/toggle_filter_controller.js
+++ app/javascript/controllers/toggle_filter_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-filter"
 export default class extends Controller {
-  static targets = ["toggleableElement"]
+  static targets = ["toggleableElement", "checkboxElement"]
   connect() {
     console.log('Connected!')
   }
@@ -11,7 +11,23 @@ export default class extends Controller {
     this.toggleableElementTarget.classList.toggle("d-none");
   }
 
-  checkboxSelect(){
-    console.log("Clicked!")
+  checkboxSelect(event){
+    // Get all the breeds that are currently selected (iterate through all the checkboxes)
+    const elements = this.checkboxElementTargets;
+    const checkedBoxes = new Array();
+    // Get a list of string with only the checked ones
+    elements.forEach(element => {
+      if (element.checked) {
+        checkedBoxes.push(element.dataset.breed);
+        console.log(checkedBoxes);
+      }
+    }
+    );
+    // Request to the backend with the filter and get all the dogs (as JSON)
+
+    // console.log("Clicked!");
+    const checkbox = event.target;
+    const isChecked = checkbox.checked;
+    console.log(checkbox.dataset.breed);
   }
 }
