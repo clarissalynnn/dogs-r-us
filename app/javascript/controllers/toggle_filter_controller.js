a57c67b2399fb42ef804bfb0d610fab4d193f69b
Clarissa Lynn <clarissalynn.5.cl@gmail.com>
2024-02-28 12:52:11 +0800

    working on the filter form

--- app/javascript/controllers/toggle_filter_controller.js
+++ app/javascript/controllers/toggle_filter_controller.js
@@ -2,6 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-filter"
 export default class extends Controller {
+  static targets =["toggleableElement"]
   connect() {
+    console.log('Connected!')
+  }
+
+  fire () {
+    this.toggleableElementTarget.classList.toggle("d-none");
   }
 }
